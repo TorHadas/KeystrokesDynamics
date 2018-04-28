752094bcffc18308f6324ddeddb3536b70e9b4e9
ZivBeker <ziv.beker@gmail.com>
2018-04-28 14:48:34 +0300

    design and names 1

--- Network.py
+++ Network.py
@@ -2,30 +2,70 @@ import numpy as np
 from Data import Data
 folder_dir = "bla_bla"
 
-iter_num = 1000
-data = Data(folder_dir)
-data = data.members_data
+damp = 0.5
 
-def sigmoid (x, deriv=False):
-    if(deriv==True):
+nodes = [
+    [],  # input
+    [],  # hidden
+    []   # output
+]
+
+bias = [
+    [],  # ->hidden
+    []   # ->output
+]
+
+weights = [
+    [],  # input -> hidden
+    []   # hidden -> output
+]
+
+delta_nodes = [[], [], []]
+delta_bias = [[], []]
+delta_weights = [[], []]
+
+# delta_weights[i][j][k] = - damp * cost_deriv[j] * activ_deriv[i][j] *
+# nodes[i][k]
+
+
+# z[i][j] = SUM_k(weights[i-1][j][k]*nodes[i-1][k]) + bias[i][j];
+# z[i][j] = weights[i-1][j]*nodes[i-1] + bias[i][j];
+# nodes[i][j] = sigmoid(z[i][j]);
+
+
+def activation_func(x, derivative=False):
+    if derivative:
         return x*(1-x)
     return 1/(1+np.exp(-x))
 
-def error_calculation(net_result, answer):
-    return np.power(net_result-answer, 2)
 
+def cost(net_result, answer, derivative=False):
+    if derivative:
+        return 2*(net_result - answer)
+    return np.power(net_result - answer, 2)
+
+
+
+
+
+
+iter_num = 1000
+data = Data(folder_dir)
+data = data.members_data
 answer_vector = []
-input_data =
+input_data = []
+
 for i in range(data.length):
     input_data.append(data[i][1])
-    ans = [0]*l0.length
-    ans[data[i][0]]=1
+    ans = [0]*input_data[i].length
+    ans[data[i][0]] = 1
     answer_vector.append(ans)
-l0 = np.array(l0)
+
+l0 = np.array(input_data[0])
 answers = np.array(answer_vector)
-l0_length = l0.length
+l0_length = len(l0)
 l1_length = 10
-l2_length = answer_vector.length
+l2_length = len(answer_vector)
 
 weight0 = 2 * np.random.random((l0_length, l1_length)) - 1
 bias0 = np.random.random_sample()
@@ -33,14 +73,18 @@ bias0 = np.random.random_sample()
 weight1 = 2 * np.random.random((l1_length, l2_length)) - 1
 bias1 = np.random.random_sample() + 1
 
-l1 = np.array([0]*l1_length)
+l1 = np.array([])
 l2 = np.array([])
 error = 0
-for i in range():
+for sample in input_data:
+    l0 = sample
     # hidden layers calculation
-    l1.add(np.dot(weight0, l0[i]) + bias0)
-    l2.add(np.dot(weight1, l1[i]) + bias1)
+    l1 = (np.dot(weight0, l0) + bias0)
+    l2 = (np.dot(weight1, l1) + bias1)
 
     # calculating error from answer
-    error += error_calculation(l2[i], answer_vector)
+    error += cost(l2, answer_vector[i])
+
+
+
 
