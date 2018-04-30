c4a73087662fe5839f342c5b56f291f62dea547a
Tor Hadas <torhadas@mail.huji.ac.il>
2018-04-30 23:11:42 +0300

    ROLLERSKOOTER

--- keylogger.js
+++ keylogger.js
@@ -4,40 +4,77 @@ var ENTER_KEY = 13;
 var BACKSPACE_KEY = 8;
 var keys = [];
 var i = 0;
-var words = {
-	"the": [], "be": [], "to": [], "of": [], "and": [], "a": [], "in": [], "that": [], "have": [], "i": [], "it": [], "for": [], "not": [], "on": [], "with": [], "he": [], "as": [], "you": [], "do": [], "at": [], "this": [], "but": [], "his": [], "by": [], "from": [], "they": [], "we": [], "say": [], "her": [], "she": [], "or": [], "an": [], "will": [], "my": [], "one": [], "all": [], "would": [], "there": [], "their": [], "what": [], "so": [], "up": [], "out": [], "if": [], "about": [], "who": [], "get": [], "which": [], "go": [], "me": [], 
-};
 
 var letancies = [];
 var password = [];
+var passwordStr = "";
 var password_latency = [];
 var inputing = true;
+/// write to file
+var txtFile = "log.txt";
+var file = new File([""], txtFile);
+function reset() {
+	timer = {};
+	keys = [];
+	i = 0;
+	letancies = [];
+	password = [];
+	passwordStr = "";
+	password_latency = [];
+	inputing = true;
+	document.getElementById("count").innerHTML = 0;
+	document.getElementById("pass").innerHTML = "--";
+
+}
+function download() {
+    var textToSaveAsBlob = new Blob([passwordStr + "\r\n" + letancies.join("\r\n")], {type:"text/plain"});
+    var textToSaveAsURL = window.URL.createObjectURL(textToSaveAsBlob);
+    var fileNameToSaveAs = "log.txt";
+
+    var downloadLink = document.createElement("a");
+    downloadLink.download = fileNameToSaveAs;
+    downloadLink.innerHTML = "Download File";
+    downloadLink.href = textToSaveAsURL;
+    downloadLink.onclick = destroyClickedElement;
+    downloadLink.style.display = "none";
+    document.body.appendChild(downloadLink);
+ 
+    downloadLink.click();
+}
+
+function destroyClickedElement(event)
+{
+    document.body.removeChild(event.target);
+}
 
 function keydown(event) {
 	var key = event.which || event.keyCode;
 	if(!timer[key]) {
-		timer[key] = performance.now();
+		timer[key] = Math.round(10*performance.now())/10;
 	}
 }
 
 function keyup(event) {
 	
 	var key = event.which || event.keyCode;
-	var latency = performance.now() - timer[key];
+	var latency = Math.round(10*(performance.now() - timer[key]))/10;
 	keys.push({"key": key, "time": timer[key], "latency": latency});
 	if(inputing) {
 		console.log("pressed "+key+"-"+String.fromCharCode(key)+" for "+latency+" seconds.");
 	}
 	if(key == ENTER_KEY) {
+		document.getElementById("Text1").value = "";
 		//console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 
 		if(inputing) {
 			console.log("~~~ PASSWORD: "+password);
 			password_latency = [];
 			inputing = false;
+			document.getElementById("pass").innerHTML = passwordStr;
 		}
 		if(password_latency.length == password.length * 2 - 1) {
 			letancies.push(password_latency);
+			document.getElementById("count").innerHTML = letancies.length
 			console.log("~~~ PUSHED");
 			//console.log("~~~ LETANCY: "+password_latency);
 			//console.log("~~~ LETANCIES: "+letancies);
@@ -47,6 +84,7 @@ function keyup(event) {
 	}
 	else if(inputing) {
 		password.push(key);
+		passwordStr += String.fromCharCode(key);
 	}
 
 	if(key == BACKSPACE_KEY || i > password.length || key != password[i]) {
@@ -58,33 +96,12 @@ function keyup(event) {
 	else {
 		if(i > 0) {
 			var time_between = timer[key] - keys[keys.length - 2].time - keys[keys.length - 2].latency;
-			password_latency.push(time_between);
+			password_latency.push(Math.round(10*time_between)/10);
 		}
 		//console.log("~~~ PUSHING LATENCY");
 		password_latency.push(latency);
 		i++;
-	}
-	
-
-	/*
-	if(key == SPACE_KEY) {
-		for(var i = 2; i < 10; i++) {
-			if(keys[keys.length - i].key == SPACE_KEY) {
-				var word = "";
-				var time = keys[keys.length - 2].time + keys[keys.length - 2].letancy - keys[keys.length - i + 1].time;
-				for(var j = keys.length-i+1; j < keys.length-1; j++) {
-					word += String.fromCharCode(keys[j].key).toLowerCase();
-				}
-				console.log("word - "+word);
-				if(words[word]) {
-					words[word].push(time);
-				}
-				break;
-			}
-		}a	e
-	*/
-
-	
+	}	
 	timer[key] = 0;
 	
 }
