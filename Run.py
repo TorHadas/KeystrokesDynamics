5788f8be213c116d3ff4701c4f900aba79cdfb02
ZivBeker <ziv.beker@gmail.com>
2018-05-09 02:14:31 +0300

    ADDED FAKE, UPGRADED TEST

--- Run.py
+++ Run.py
@@ -1,12 +1,10 @@
 from NetworkClass import Network
 import os
-<<<<<<< HEAD
 import Data
-=======
 import numpy as np
 from plotResults import plot
+from NetworkClass import cost
 
->>>>>>> 7ce3f0af3429120c0d279bf7c9582b127ac5c9b7
 folder_dir = os.getcwd() + "\\logs"
 
 # network constants
@@ -22,16 +20,12 @@ LAYER_SIZE = [
 
 network = Network(DAMP, DEFAULT_ITER_NUM, NETWORK_SIZE, LAYER_SIZE)
 network.get_data(folder_dir)
-<<<<<<< HEAD
 network.train()
 network.save("test.json")
 net = Network(DAMP, DEFAULT_ITER_NUM, NETWORK_SIZE, LAYER_SIZE)
 net.load("test.json")
 net.run(Data.vec_to_data([71.7,128.1,120.1,95.8,96.1,-16.1,104.1,200,79.9,624.2,119.8,744.8,95.3,64.5,96.1]))
 
-
-=======
-network.train_network()
 
 def identification_decider(result):
     maximum = 0
@@ -43,16 +37,34 @@ def identification_decider(result):
     result_answer[index] = 1
     return result_answer
 
+def get_data(network, folder_data_dir):
+    orig_data = Data.create_multiple_members(folder_data_dir)
+    #orig_data = Data.get_fake_data()
+    answers = np.zeros((len(orig_data), LAYER_SIZE[network.L]))
+    inputs = []
+    new_data = []
+    for i in range(len(orig_data)):
+        answers[i][orig_data[i][0]] = 1
+        inputs.append(orig_data[i][1])
+        new_data.append([answers[i], inputs[-1]])
+    data = [answers, inputs]
+    return new_data
 
-def test(network, data):
+
+def test(network, folder_data):
+    data = get_data(network, folder_data)
+    data = data[(int)(len(data) / 10) : len(data)]
     imposter = 1; othentic = 0
     true_identifications = 0; false_identification = 0
     false_rejection_rate = 0; false_acceptance_rate = 0
     mean_cost_vec = []; max_cost_vec = []
+    print(data)
+
     for test_element in data:
         net_result = network.run(test_element[1])
-        mean_cost_vec.append((sum(network.cost(net_result, test_element[0])) / len(test_element)) ** (1/2))
-        max_cost_vec.append(max(network.cost(net_result, test_element[0])) ** (1/2))
+        print(net_result)
+        mean_cost_vec.append((sum(cost(net_result, test_element[0])) / len(test_element)) ** (1/2))
+        max_cost_vec.append(max(cost(net_result, test_element[0])) ** (1/2))
         net_decision = identification_decider(net_result)
         if net_decision == test_element[0]:
             true_identifications += 1
@@ -72,4 +84,5 @@ def test(network, data):
     true_identifications_pct = true_identifications / len(data)
     print("frr is: " + str(false_rejection_rate) +  " far is: " + str(false_acceptance_rate) +
           " good: " + str(true_identifications_pct))
->>>>>>> 7ce3f0af3429120c0d279bf7c9582b127ac5c9b7
+
+test(network, folder_dir)
