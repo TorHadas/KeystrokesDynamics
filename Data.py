071080a8cf02d7d07bba8ddb2087a44fdfcf12f3
Tor Hadas <torhadas@mail.huji.ac.il>
2018-05-09 02:16:45 +0300

    added .self, changed names

--- Data.py
+++ Data.py
@@ -3,7 +3,7 @@ import math
 import numpy as np
 import re
 
-def get_numbers_data():
+def get_pictures():
     FILe = os.getcwd() + "\\data.txt"
     inputs = []
     answers = []
@@ -46,23 +46,22 @@ def get_fake_data():
 
     return data
 
-def create_person_data(directory_of_file):
+#return the password and the times for a file
+def readfile(filename):
     password = ''
-    inputs = []
-    with open(directory_of_file, 'r') as file:
+    vectors = []
+    with open(filename, 'r') as file:
         lines = file.readlines()
         password = (lines[0].split('\n'))[0]
-        lines = lines[1:]
-        for line in lines:
-            if (line is not '\n'):
-                new_row = re.split('; |,|\*|\n',line)
-                line = line.split("\n")[0].split(",")
-                data = vec_to_data(line)
-                #print("DATA ---- \t" + str(data))
-                inputs.append(data)
-    return [password, inputs]
+        for line in lines[1:]:
+            if (line is '\n'):
+                continue
+            vec = line.split("\n")[0].split(",")
+            vectors.append(normalize(vec))
+    return [password, vectors]
 
-def vec_to_data(line):
+#change the initial vector to a normilized vector
+def normalize(line):
     data = np.array(line)
     data = np.asfarray(data, float)
     # MEAN
@@ -74,33 +73,49 @@ def vec_to_data(line):
     latency_mean = sum([data[2*i + 1] for i in range(math.floor((len(data))/2))]) / (len(data)/2)
     return np.concatenate((np.array([mean, press_mean, latency_mean]), data))
 
-def create_multiple_members(folder_directory):
-    members = []
-    for file in os.listdir(folder_directory):
+def get_data():
+    return get_users(str(os.getcwd())+"\\logs\\")
+
+def get_users(folder):
+    users = []
+    for file in os.listdir(folder):
         inserted = False
-        file_path = os.path.relpath("logs\\" + file)
-        if(file_path == "logs\passwords.txt"):
+        file_path = "logs\\" + file
+        if(file_path == "logs\\passwords.txt"):
             continue
-        new_member = create_person_data(file_path)
-        #for i in range(len(members)):
-        #    if new_member[0] == members[i][0]:
-        #        inserted = True
-        #        members[i][1] += new_member[1]
-        if not inserted:
-            members.append(new_member)
-    return convert_members(members)
+        user = readfile(file_path)
+        user = [user[0], user[1], []]
+        users.append(user)
+    users = get_fake_users(folder + "fake", users)
+    users = [to_tuples(users[i]) for i in range(len(users))]
+    return to_tuples(users)
+
+def get_fake_users(folder, users):
+    for file in os.listdir(folder):
+        file_path = "logs\\fake\\" + file
+        imposter = readfile(file_path)
+        for i in range(len(users)):
+            if imposter[0] == users[i][0]:
+                users[i][2] += imposter[1]
+    return users
 
 
-def convert_members(members):
-    converted_members = []
+# def to_tuples(users):
+#     converted_users = []
+#     for i in range(2):
+#         for j in range(len(users[i][1])):
+#             converted_users.append((i, users[i][1][j]))
+#     return converted_users
+
+def to_tuples(user):
+    data = []
     for i in range(2):
-        #for i in range(len(members)):
-        for j in range(len(members[i][1])):
-            converted_members.append((i, members[i][1][j]))
-    return converted_members
+        for j in range(len(user[i + 1])):
+            data.append((i, users[i][j]))
+    return data
 
 #file_dir = "C:\\Users\\T8497069\\Desktop\\bloch 1.txt"
-#person1_data = create_person_data(file_dir)
+#person1_data = readfile(file_dir)
 #print(len(person1_data))
 #print(len(person1_data[1]))
 #print(person1_data[1])
--- NetworkClass.py
+++ NetworkClass.py
@@ -32,20 +32,9 @@ class Network():
         self.NETWORK_SIZE = network_size
         self.L = network_size - 1
         self.nodes = [np.zeros(self.LAYER_SIZE[i]) for i in range(self.L + 1)]
-        self.weights = [2 * np.random.rand(self.LAYER_SIZE[i + 1], self.LAYER_SIZE[i]) - 1 for i in range(self.L)]
-        self.bias = [2 * np.random.rand(self.LAYER_SIZE[i + 1]) - 1 for i in range(self.L)]
+        self.weights = [0.01 * (np.random.rand(self.LAYER_SIZE[i + 1], self.LAYER_SIZE[i]) - 0.5) for i in range(self.L)]
+        self.bias    = [0.01 * (np.random.rand(self.LAYER_SIZE[i + 1]) - 0.5) for i in range(self.L)]
 
-        self.total_delta_weights = [np.zeros((self.LAYER_SIZE[i + 1], self.LAYER_SIZE[i])) for i in range(self.L)]
-        self.total_delta_bias = [np.zeros(self.LAYER_SIZE[i + 1]) for i in range(self.L)]
-
-        self.delta_nodes = list(self.nodes)
-        self.delta_bias = list(self.bias)
-        self.delta_weights = list(self.weights)
-
-        self.z = list(self.nodes)
-        self.delta_z = list(self.nodes)
-        self.total_delta_weights = list(self.weights)
-        self.total_delta_bias = list(self.bias)
         self.network_train_counter = 0
 
     def save(self, filename):
@@ -67,10 +56,12 @@ class Network():
     
     def run(self, sample):
         self.nodes[0] = activation_func(sample)
+        #self.nodes[0] = sample
+        z = list(self.nodes)
         for i in range(1, self.L + 1):
-            self.z[i] = np.dot(self.weights[i - 1], self.nodes[i - 1]) + self.bias[i - 1]
-            self.nodes[i] = activation_func(self.z[i])
-        return self.nodes[self.L]
+            z[i] = np.dot(self.weights[i - 1], self.nodes[i - 1]) + self.bias[i - 1]
+            self.nodes[i] = activation_func(z[i])
+        return self.nodes[self.L], z
 
     def get_data(self, folder_data_dir):
         orig_data = Data.create_multiple_members(folder_data_dir)
@@ -88,56 +79,71 @@ class Network():
         if(iter_num == None):
             iter_num = self.DEFAULT_ITER_NUM
         # region: setting variables to be the same as the class variables
-        L = self.L; LAYER_SIZE = self.LAYER_SIZE; inputs = self.inputs; answers = self.answers;
-        nodes = self.nodes; z = self.z; delta_z = self.delta_z; delta_bias = self.delta_bias; 
-        delta_weights = self.delta_weights; delta_nodes = self.delta_nodes; DAMP = self.DAMP; 
-        weights = self.weights; total_delta_weights = self.total_delta_weights; bias = self.bias; 
-        total_delta_bias = self.total_delta_bias; answers = self.answers;
+
+        total_delta_weights = [np.zeros((self.LAYER_SIZE[i + 1], self.LAYER_SIZE[i])) for i in range(self.L)]
+        total_delta_bias = [np.zeros(self.LAYER_SIZE[i + 1]) for i in range(self.L)]
+
+        delta_nodes = list(self.nodes)
+        delta_bias = list(self.bias)
+        delta_weights = list(self.weights)
+
+        z = list(self.nodes)
+        delta_z = list(self.nodes)
+        total_delta_weights = list(self.weights)
+        total_delta_bias = list(self.bias)
         #endregion
 
         # region: setting plot cost vectors
         eff_cost_vec = []
-        for i in range(LAYER_SIZE[L]):
+        for i in range(self.LAYER_SIZE[self.L]):
             eff_cost_vec.append([0] * iter_num)
-        samples_same_ans = [0] * LAYER_SIZE[L]
+        samples_same_ans = [0] * self.LAYER_SIZE[self.L]
+
+        max_cost_vec = []
+        for i in range(self.LAYER_SIZE[self.L]):
+            max_cost_vec.append([0] * iter_num)
+
+        max_vec = []
+        for i in range(self.LAYER_SIZE[self.L]):
+            max_vec.append([0 for i in range(self.LAYER_SIZE[self.L])] * iter_num)
+
+        wrong_vec = []
+        for i in range(self.LAYER_SIZE[self.L]):
+            wrong_vec.append([0] * iter_num)
         # endregion
 
-        self.debug.log(answers)
         for iter in range(iter_num):
-            ef_cost = np.zeros(LAYER_SIZE[L])
-            max_cost = np.zeros(LAYER_SIZE[L])
-            '''
-            combined = list(zip(inputs, answers))
+            ef_cost = np.zeros(self.LAYER_SIZE[self.L])
+            max_cost = np.zeros(self.LAYER_SIZE[self.L])
+            
+            combined = list(zip(self.inputs, self.answers))
             random.shuffle(combined)
-            inputs[:], answers[:] = zip(*combined)
-            '''
-            self.debug.log(answers)
+            inputs, answers = zip(*combined)
+
             for j in range(len(inputs)):
                 sample = np.array(inputs[j])
                 answer = np.array(answers[j])
 
                 #CALCULATE NODES
-                nodes[0] = activation_func(sample)
-                for i in range(1, L + 1):
-                    z[i] = np.dot(weights[i - 1], nodes[i - 1]) + bias[i - 1]
-                    nodes[i] = activation_func(z[i])
+                a, z = self.run(sample)
 
-                ef_cost += cost(nodes[L], answer) / len(inputs)
-                max_cost = np.array([max(m, curr) for m, curr in zip(max_cost, cost(nodes[L], answer))])
+
+                ef_cost += cost(self.nodes[self.L], answer) / len(inputs)
+                max_cost = np.array([max(m, curr) for m, curr in zip(max_cost, cost(self.nodes[self.L], answer))])
 
                 #LEARN
 
-                delta_z[L] = cost(nodes[L], answer, True) * activation_func(z[L], True)
-                for i in range(1, L + 1):
-                    delta_bias[L - i] = np.array(delta_z[L + 1 - i])
-                    delta_weights[L - i] = np.outer(delta_z[L + 1 - i], nodes[L - i])
-                    delta_nodes[L - i] = np.dot(delta_z[L + 1 - i], delta_weights[L - i]) / LAYER_SIZE[L - i + 1]
-                    delta_z[L - i] = delta_nodes[L - i] * activation_func(z[L - i], True)
+                delta_z[self.L] = cost(self.nodes[self.L], answer, True) * activation_func(z[self.L], True)
+                for i in range(1, self.L + 1):
+                    delta_bias[self.L - i] = np.array(delta_z[self.L + 1 - i])
+                    delta_weights[self.L - i] = np.outer(delta_z[self.L + 1 - i], self.nodes[self.L - i])
+                    delta_nodes[self.L - i] = np.dot(delta_z[self.L + 1 - i], delta_weights[self.L - i]) / self.LAYER_SIZE[self.L - i + 1]
+                    delta_z[self.L - i] = delta_nodes[self.L - i] * activation_func(z[self.L - i], True)
 
                 
                 if(j == 1 or j == len(inputs)-1):
-                    self.debug.log(nodes[L])
-                    self.debug.log(answer)
+                    self.debug.log("j="+str(j)+", "+str(self.nodes[self.L]))
+                    self.debug.log("answer = "+str(answer))
 
                 total_delta_weights = [np.nan_to_num(tdw + dw) for tdw, dw in zip(total_delta_weights, delta_weights)]
                 total_delta_bias    = [np.nan_to_num(tdb + db) for tdb, db in zip(total_delta_bias, delta_bias)]
@@ -146,33 +152,42 @@ class Network():
                 for i in range(len(answer)):
                     if (answer[i] == 1):
                         # self.debug.log("in input j: " + str(j) + " the answer is " + str(
-                        #    answer) + " the net answer is " + str(nodes[L]) +
+                        #    answer) + " the net answer is " + str(self.nodes[self.L]) +
                         #      " adding cost to place " + str(i) + " " + str(iter))
                         if (iter == 0):
                             samples_same_ans[i] += 1
-                        eff_cost_vec[i][iter] += ((sum(cost(nodes[L], answer)) / LAYER_SIZE[L]) ** (1 / 2))
+                        eff_cost_vec[i][iter] += ((sum(cost(self.nodes[self.L], answer)) / self.LAYER_SIZE[self.L]) ** (1 / 2))
+                        
+                        if(max_cost_vec[i][iter] < sum(cost(self.nodes[self.L], answer))):
+                            max_cost_vec[i][iter] = sum(cost(self.nodes[self.L], answer))
+                            max_vec[i][iter] = self.nodes[self.L]
+
+                        if(sum(cost(self.nodes[self.L], answer)) > 0.1):
+                            wrong_vec[i][iter] += 1
                 # endregion
 
-            weights = [np.nan_to_num(w - DAMP * dw / len(inputs)) for w, dw in zip(weights, total_delta_weights)]
-            bias    = [np.nan_to_num(b - DAMP * db / len(inputs)) for b, db in zip(bias, total_delta_bias)]
+            self.weights = [np.nan_to_num(w - self.DAMP * dw / len(inputs)) for w, dw in zip(self.weights, total_delta_weights)]
+            self.bias    = [np.nan_to_num(b - self.DAMP * db / len(inputs)) for b, db in zip(self.bias, total_delta_bias)]
 
             
-            #self.debug.log("TOTAL DELTA="+str(DAMP*total_delta_weights[L-1] / len(inputs)))
+            #self.debug.log("TOTAL DELTA="+str(DAMP*total_delta_weights[self.L-1] / len(inputs)))
             
             # normalize by num of samples
             for i in range(len(eff_cost_vec)):
                 eff_cost_vec[i][iter] = eff_cost_vec[i][iter] / samples_same_ans[i]
                 # endregion
+            self.debug.log(max_cost_vec[0][iter])
+            self.debug.log("i=0, max="+str(max_vec[0][iter]))
 
         # region: plot graphs
         iter_vec = []
         for i in range(len(eff_cost_vec)):
             iter_vec.append(np.arange(len(eff_cost_vec[i])))
-        simplePlot(iter_vec, eff_cost_vec, DAMP)
+        simplePlot(iter_vec, eff_cost_vec, self.DAMP)
+        simplePlot(iter_vec, wrong_vec, self.DAMP)
         # endregion
 
-        self.L = L; self.LAYER_SIZE = LAYER_SIZE; self.inputs = inputs; self.answers = answers; self.nodes = nodes; self.z = z; self.delta_z = delta_z; self.delta_bias = delta_bias; self.delta_weights = delta_weights; self.delta_nodes = delta_nodes; self.DAMP = DAMP; self.weights = weights; self.total_delta_weights = total_delta_weights; self.bias = bias; self.total_delta_bias = total_delta_bias; self.answers = answers; 
-        return weights, bias, LAYER_SIZE
+        return self.weights, self.bias, self.LAYER_SIZE
 
 
 def array_to_list(arr):
